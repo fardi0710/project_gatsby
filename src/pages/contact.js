19720e855f1578160e6179b07ca8f220856617b6
fardi0710 <fardi.hamdan@gmail.com>
2023-10-27 21:46:31 +0700

    hapus contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,7 @@ return (
 	<div>
 		<h1>Contact</h1>
 		<p>Saya bisa dihub via @fardi.hamdan di instagram!</p>
-		<p>Saya bisa dihub via  <href="https://instagram.com/fardi.hamdan" target="_blank">@fardi.hamdan</a> di instagram!</p>
+
 	</div>
 )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import * as React from "react"
 
-import { Link } from 'gatsby'
-
 const IndexPage = () => {
   return (
         <div>
@@ -9,7 +7,7 @@ const IndexPage = () => {
 	  <h2>Selamat Malam rekan-rekan LDB</h2>
 	  <p>Berikut Jadwal line Dispatc Briefing bulan November 2023</p>
 	  <p>Kurang jelas dengan jadwal hub saya! <a href="/contact">Hubungi saya</a></p>
-	  <p>Mau request Cuti Silahkan hub <Link to="/contact">Hubungi saya.</Link></p>
+
 	<div>
 )
 }
