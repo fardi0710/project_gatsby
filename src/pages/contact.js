304488de210141d80a0f216b4ed8e711bdc0f2ae
fardi0710 <fardi.hamdan@gmail.com>
2023-10-27 21:24:25 +0700

    edit index dan tambah file contact

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactPage = () => {
+return (
+	<div>
+		<h1>Contact</h1>
+		<p>Saya bisa dihub via @fardi.hamdan di instagram!</p>
+		<p>Saya bisa dihub via  <href="https://instagram.com/fardi.hamdan" target>
+		<footer />
+	</div>
+)
+}
+export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import * as React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+import { Link } from 'gatsby'
+
+const IndexPage = () => {
+  return (
+        <div>
+	  <h1>Assalamualaikum wr wb</h1>
+	  <h2>Selamat Malam rekan-rekan LDB</h2>
+	  <p>Berikut Jadwal line Dispatc Briefing bulan November 2023</p>
+	  <p>Kurang jelas dengan jadwal hub saya! <a href="/contact">Hubungi saya</a></p>
+	  <p>Mau request Cuti Silahkan hub <Link to="/contact">Hubungi saya.</Link></p>
+	<div>
+)
 }
+export default indexPage
