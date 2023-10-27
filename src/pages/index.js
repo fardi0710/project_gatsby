30bfacb25d8b8edb00990edd5ceacb0d4da6e615
fardi0710 <fardi.hamdan@gmail.com>
2023-10-27 21:54:28 +0700

    hapus contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,6 @@ const IndexPage = () => {
 	  <h1>Assalamualaikum wr wb</h1>
 	  <h2>Selamat Malam rekan-rekan LDB</h2>
 	  <p>Berikut Jadwal line Dispatc Briefing bulan November 2023</p>
-	  <p>Kurang jelas dengan jadwal hub saya! <a href="/contact">Hubungi saya</a></p>
-
 	<div>
 )
 }
